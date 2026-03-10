6f619d5d6afca45eceba92f679316e31146201d2
emilienmaudet <emilien.maudet@gmail.com>
2026-03-10 17:50:46 +0100

    Initial commit

---
+++ DemoRunnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoRunnerApp.swift
+//  DemoRunner
+//
+//  Created by emilien maudet on 10/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct DemoRunnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
