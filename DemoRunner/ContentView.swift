682ba5674cd8e9146dc5dc2794758ef39370f7a5
emilienmaudet <emilien.maudet@gmail.com>
2026-03-11 11:17:15 +0100

    fix

--- DemoRunner/ContentView.swift
+++ DemoRunner/ContentView.swift
@@ -14,7 +14,9 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+                .font(.largeTitle)
         }
+        .frame(minWidth: 400, minHeight: 300)
         .padding()
     }
 }
